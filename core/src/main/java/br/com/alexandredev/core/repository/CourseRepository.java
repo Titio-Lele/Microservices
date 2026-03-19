19a72494319e4c2f28acc96c4c1732b0bb61f871
Alexandre A Santos <dreadlord@localhost.localdomain>
2026-03-19 00:35:47 -0300

    refactor: migrate core and log code to Boot 3 and Jakarta APIs

--- core/src/main/java/br/com/alexandredev/core/repository/CourseRepository.java
+++ core/src/main/java/br/com/alexandredev/core/repository/CourseRepository.java
@@ -1,7 +1,7 @@
 package br.com.alexandredev.core.repository;
 
 import br.com.alexandredev.core.model.Course;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
